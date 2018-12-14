6e5999b662478dfffda9d117afad5c25c9d2b08d
KanHarI <knaan.harpaz@gmail.com>
2018-12-14 23:19:59 +0200

    .

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(sbm.parameters(), lr=0.001, momentum=0.9)
 
 
-for epoch in range(2):  # loop over the dataset multiple times
+for epoch in range(5):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
