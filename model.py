af3e289415488bc1881668a4aa06c6f44bc48706
KanHarI <knaan.harpaz@gmail.com>
2018-12-14 22:57:53 +0200

    For cloud

--- model.py
+++ model.py
@@ -10,33 +10,32 @@ import math
 import random
 
 
-
 def conv3(channels):
-	return nn.Conv2d(channels, channels, 3, padding=1)
+    return nn.Conv2d(channels, channels, 3, padding=1)
 
 def conv5(channels):
-	return nn.Conv2d(channels, channels, 5, padding=2)
+    return nn.Conv2d(channels, channels, 5, padding=2)
 
 def dep_conv5(channels):
-	return nn.Conv2d(channels, channels, 5, padding=2, groups=channels)
+    return nn.Conv2d(channels, channels, 5, padding=2, groups=channels)
 
 def dep_conv7(channels):
-	return nn.Conv2d(channels, channels, 7, padding=3, groups=channels)
+    return nn.Conv2d(channels, channels, 7, padding=3, groups=channels)
 
 def diaconv3_2(channels):
-	return nn.Conv2d(channels, channels, 3, padding=2, dilation=2)
+    return nn.Conv2d(channels, channels, 3, padding=2, dilation=2)
 
 def diaconv5_2(channels):
-	return nn.Conv2d(channels, channels, 5, padding=4, dilation=2)
+    return nn.Conv2d(channels, channels, 5, padding=4, dilation=2)
 
 def maxpool3(*_):
-	return nn.MaxPool2d(3, stride=1, padding=1)
+    return nn.MaxPool2d(3, stride=1, padding=1)
 
 def maxpool5(*_):
-	return nn.MaxPool2d(5, stride=1, padding=2)
+    return nn.MaxPool2d(5, stride=1, padding=2)
 
 def maxpool7(*_):
-	return nn.MaxPool2d(7, stride=1, padding=3)
+    return nn.MaxPool2d(7, stride=1, padding=3)
 
 
 ACTIVATIONS = [conv3, conv5, dep_conv5, dep_conv7, diaconv3_2, diaconv5_2, maxpool3, maxpool5, maxpool7]
@@ -49,111 +48,114 @@ SUBMODEL_CHANNELS = 10
 IMAGE_CHANNELS = 3
 
 class Supermodel(nn.Module):
-	def __init__(self, graphsage_conv_layers=GRAPHSAGE_LAYERS, activations_list=ACTIVATIONS, max_size=256, max_halvings=8):
-		super().__init__()
-		self.max_size = max_size
-		self.max_halvings = max_halvings
-		self.log2_max_size = math.ceil(math.log2(max_size))
-		self.activations_list = activations_list
-		# +2 for input, output nodes
-		self.input_feature_sizes = 2 + len(activations_list) + self.log2_max_size*2 + max_halvings*2
-		self.graphsage = gs.PyramidGraphSage(
-			graphsage_conv_layers,
-			[self.input_feature_sizes] + [GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers,
-			[GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers)
-		
-		# +1 for priority, + max_halvings for amount of dimensional halvings
-		node_output_feature_sizes = len(activations_list) + 1
-		self.node_processor = nn.Linear(self.input_feature_sizes + GRAPHSAGE_REPRESENTATION_SIZE, node_output_feature_sizes)
+    def __init__(self, graphsage_conv_layers=GRAPHSAGE_LAYERS, activations_list=ACTIVATIONS, max_size=256, max_halvings=8):
+        super().__init__()
+        self.max_size = max_size
+        self.max_halvings = max_halvings
+        self.log2_max_size = math.ceil(math.log2(max_size))
+        self.activations_list = activations_list
+        # +2 for input, output nodes
+        self.input_feature_sizes = 2 + len(activations_list) + self.log2_max_size*2 + max_halvings*2
+        self.actor_graphsage = gs.PyramidGraphSage(
+            graphsage_conv_layers,
+            [self.input_feature_sizes] + [GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers,
+            [GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers)
+        self.critic_graphsage = gs.PyramidGraphSage(
+            graphsage_conv_layers,
+            [self.input_feature_sizes] + [GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers,
+            [GRAPHSAGE_REPRESENTATION_SIZE] * graphsage_conv_layers)
+        
+        # +1 for priority, + max_halvings for amount of dimensional halvings
+        node_output_feature_sizes = len(activations_list) + 1
+        self.node_processor = nn.Linear(self.input_feature_sizes + GRAPHSAGE_REPRESENTATION_SIZE, node_output_feature_sizes)
 
-		# inputs: +current distance, +2 for current connectedness
-		# outputs: priority, connectedeness [yes\no]
-		self.pair_selector_1 = nn.Linear(self.input_feature_sizes*2 + GRAPHSAGE_REPRESENTATION_SIZE*2 + self.log2_max_size + 2, 1 + 2)
-		self.pair_selector_2 = nn.Linear(self.input_feature_sizes*2 + GRAPHSAGE_REPRESENTATION_SIZE*2 + self.log2_max_size + 2, 1 + 2)
+        # inputs: +current distance, +2 for current connectedness
+        # outputs: priority, connectedeness [yes\no]
+        self.pair_selector_1 = nn.Linear(self.input_feature_sizes*2 + GRAPHSAGE_REPRESENTATION_SIZE*2 + self.log2_max_size + 2, 1 + 2)
+        self.pair_selector_2 = nn.Linear(self.input_feature_sizes*2 + GRAPHSAGE_REPRESENTATION_SIZE*2 + self.log2_max_size + 2, 1 + 2)
 
-	def cuda(self):
-		self.graphsage = self.graphsage.cuda()
-		self.node_processor = self.node_processor.cuda()
-		self.pair_selector = self.pair_selector.cuda()
-		return self
+    def cuda(self):
+        self.actor_graphsage = self.actor_graphsage.cuda()
+        self.node_processor = self.node_processor.cuda()
+        self.pair_selector = self.pair_selector.cuda()
+        return self
 
-	def create_submodel(self, submodel_size, layers_between_halvings, output_dim, channels=SUBMODEL_CHANNELS, inp_channels=IMAGE_CHANNELS):
-		return Submodel(submodel_size, channels, self, layers_between_halvings, output_dim, inp_channels)
+    def create_submodel(self, submodel_size, layers_between_halvings, output_dim, channels=SUBMODEL_CHANNELS, inp_channels=IMAGE_CHANNELS):
+        return Submodel(submodel_size, channels, self, layers_between_halvings, output_dim, inp_channels)
 
 
 
 class Submodel(nn.Module):
-	def __init__(self, size, channels, supermodel, layers_between_halvings, output_dim, inp_channels):
-		super().__init__()
-		self.size = size
-		self.channels = channels
-		self.supermodel = supermodel
-		self.layers_between_halvings = layers_between_halvings
-		self.supergraph = sg.Supergraph(size, channels, self.supermodel.activations_list, layers_between_halvings, inp_channels)
-		self.adj_matrix = torch.zeros(size, size)
+    def __init__(self, size, channels, supermodel, layers_between_halvings, output_dim, inp_channels):
+        super().__init__()
+        self.size = size
+        self.channels = channels
+        self.supermodel = supermodel
+        self.layers_between_halvings = layers_between_halvings
+        self.supergraph = sg.Supergraph(size, channels, self.supermodel.activations_list, layers_between_halvings, inp_channels)
+        self.adj_matrix = torch.zeros(size, size)
 
-		# Build skip connections automatically
-		for i in range(0,size-1):
-			self.adj_matrix[i,i+1] = 1
-			if i < size-i-1:
-				self.adj_matrix[i,size-i-1] = 1
+        # Build skip connections automatically
+        for i in range(0,size-1):
+            self.adj_matrix[i,i+1] = 1
+            if i < size-i-1:
+                self.adj_matrix[i,size-i-1] = 1
 
-		# All initialized to first possible activation function...
-		self.chosen_activations = torch.zeros(size, dtype=torch.int)
-		for i in range(1,size-1):
-			self.chosen_activations[i] = random.randint(0,len(self.supermodel.activations_list)-1)
+        # All initialized to first possible activation function...
+        self.chosen_activations = torch.zeros(size, dtype=torch.int)
+        for i in range(1,size-1):
+            self.chosen_activations[i] = random.randint(0,len(self.supermodel.activations_list)-1)
 
-		self.subgraph = self.supergraph.create_subgraph(self.chosen_activations, self.adj_matrix)
-		self.final_classifier = nn.Linear(channels*(2**((size-1)//layers_between_halvings)), output_dim)
+        self.subgraph = self.supergraph.create_subgraph(self.chosen_activations, self.adj_matrix)
+        self.final_classifier = nn.Linear(channels*(2**((size-1)//layers_between_halvings)), output_dim)
 
-	def cuda(self):
-		self.supergraph = self.supergraph.cuda()
-		self.adj_matrix = self.adj_matrix.cuda()
-		self.chosen_activations = self.chosen_activations.cuda()
-		self.subgraph = self.subgraph.cuda()
-		self.final_classifier = self.final_classifier.cuda()
-		return self
+    def cuda(self):
+        self.supergraph = self.supergraph.cuda()
+        self.adj_matrix = self.adj_matrix.cuda()
+        self.chosen_activations = self.chosen_activations.cuda()
+        self.subgraph = self.subgraph.cuda()
+        self.final_classifier = self.final_classifier.cuda()
+        return self
 
-	def refresh_subgraph(self):
-		nodes = torch.zeros(self.size, self.supermodel.input_feature_sizes)
-		for i in range(self.size):
-			# Locations
-			if i == 0:
-				nodes[i,0] = 1
-			elif i == self.size-1:
-				nodes[i,1] = 1
-			else:
-				nodes[i,2 + self.chosen_activations[i]] = 1
+    def refresh_subgraph(self):
+        nodes = torch.zeros(self.size, self.supermodel.input_feature_sizes)
+        for i in range(self.size):
+            # Locations
+            if i == 0:
+                nodes[i,0] = 1
+            elif i == self.size-1:
+                nodes[i,1] = 1
+            else:
+                nodes[i,2 + self.chosen_activations[i]] = 1
 
-			# Add position pointers
-			ptr_for = 2 + len(self.chosen_activations)
-			ptr_rev = ptr_for + self.supermodel.log2_max_size
-			for i in range(self.size):
-				for_rep = ('0'*self.supermodel.log2_max_size + bin(i)[2:])[-self.supermodel.log2_max_size:]
-				rev_rep = ('0'*self.supermodel.log2_max_size + bin(self.size-1-i)[2:])[-self.supermodel.log2_max_size:]
-				for j in range(self.supermodel.log2_max_size):
-					if for_rep[j] == '1':
-						nodes[i, ptr_for + j] = 1
-					if rev_rep[j] == '1':
-						nodes[i, ptr_rev + j] = 1
+            # Add position pointers
+            ptr_for = 2 + len(self.chosen_activations)
+            ptr_rev = ptr_for + self.supermodel.log2_max_size
+            for i in range(self.size):
+                for_rep = ('0'*self.supermodel.log2_max_size + bin(i)[2:])[-self.supermodel.log2_max_size:]
+                rev_rep = ('0'*self.supermodel.log2_max_size + bin(self.size-1-i)[2:])[-self.supermodel.log2_max_size:]
+                for j in range(self.supermodel.log2_max_size):
+                    if for_rep[j] == '1':
+                        nodes[i, ptr_for + j] = 1
+                    if rev_rep[j] == '1':
+                        nodes[i, ptr_rev + j] = 1
 
-			# Add halving num
-			ptr_for = ptr_rev + self.supermodel.log2_max_size
-			ptr_rev = ptr_for + self.supermodel.max_halvings
-			for i in range(self.size):
-				nodes[i, ptr_for + i//self.layers_between_halvings] = 1
-				nodes[i, ptr_rev + ((self.size-1) // self.layers_between_halvings) - i // self.layers_between_halvings] = 1
+            # Add halving num
+            ptr_for = ptr_rev + self.supermodel.log2_max_size
+            ptr_rev = ptr_for + self.supermodel.max_halvings
+            for i in range(self.size):
+                nodes[i, ptr_for + i//self.layers_between_halvings] = 1
+                nodes[i, ptr_rev + ((self.size-1) // self.layers_between_halvings) - i // self.layers_between_halvings] = 1
 
-		graphsage_res = self.supermodel.graphsage((torch.stack([nodes]), torch.stack([self.adj_matrix])))[0]
-		node_processor_inp = torch.cat([nodes, graphsage_res], dim=-1)
-		node_processor_out = self.supermodel.node_processor(node_processor_inp)
-		priority = torch.exp(node_processor_out[:,0])
-		node_processor_out = node_processor_out[:,1:]
+        graphsage_res = self.supermodel.actor_graphsage((torch.stack([nodes]), torch.stack([self.adj_matrix])))[0]
+        node_processor_inp = torch.cat([nodes, graphsage_res], dim=-1)
+        node_processor_out = self.supermodel.node_processor(node_processor_inp)
+        priority = torch.exp(node_processor_out[:,0])
+        node_processor_out = node_processor_out[:,1:]
 
 
-
-	def forward(self, inp):
-		# Average over last dimension
-		inp = self.subgraph(inp).mean(-1).mean(-1)
-		inp = self.final_classifier(inp)
-		return inp
+    def forward(self, inp):
+        # Average over last dimension
+        inp = self.subgraph(inp).mean(-1).mean(-1)
+        inp = self.final_classifier(inp)
+        return inp
---
+++ test.py
@@ -0,0 +1,67 @@
+
+import model
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+import torch.optim as optim
+
+########################################################################
+# The output of torchvision datasets are PILImage images of range [0, 1].
+# We transform them to Tensors of normalized range [-1, 1].
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+supermodel = model.Supermodel()
+
+SUBMODEL_LAYERS = 10
+LAYERS_BETWEEN_HALVINGS = 4
+OUTPUT_DIM = 10
+
+sbm = supermodel.create_submodel(SUBMODEL_LAYERS, LAYERS_BETWEEN_HALVINGS, OUTPUT_DIM)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(sbm.parameters(), lr=0.001, momentum=0.9)
+
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = sbm(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 10 == 9:
+            print('[%d, %5d] loss: %f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
